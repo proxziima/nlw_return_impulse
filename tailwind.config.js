ca9648bcd1a1733bc2a1fc6be89d3b57ca701142
viniciusfqueiroz <felipevs.queiroz@gmail.com>
2022-05-07 16:05:01 -0300

    :tada: first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: "#8257E5",
+          300: "#996DFF",
+        },
+      },
+      borderRadius: {
+        md: "4px",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar")],
+};
